36b777de7752bd0445db89f4f1ca1121d35bbaae
ktournereau <kevin.tournereau@gmail.com>
2014-01-14 10:24:10 +0100

    Factorisation du code

--- app/models/NombreDecimal.java
+++ app/models/NombreDecimal.java
@@ -13,10 +13,23 @@ public class NombreDecimal {
     }
 
     public String en_romain() {
+
+    	ArrayList<String> convert = new ArrayList<String>();
+
+    	convert.add(0,"");
+    	convert.add(1,"I");
+    	convert.add(2,"II");
+    	convert.add(3,"III");
+    	convert.add(4,"IV");
+
+    	return convert.get(this.valeur);
+
+/*
 		if(this.valeur==1){	return "I";}
 		else if(this.valeur ==2){ return "II";}
 		else if(this.valeur == 3){ return "III";}
 		else if(this.valeur == 4){ return "IV";}
 		else{ return "";}
+*/
     }
 }
