4097e4fa50e1c239168716160bb75daa65a94741
ktournereau <kevin.tournereau@gmail.com>
2014-01-14 10:12:03 +0100

    Implementation pour convertir 4 en IV

--- app/models/NombreDecimal.java
+++ app/models/NombreDecimal.java
@@ -13,17 +13,10 @@ public class NombreDecimal {
     }
 
     public String en_romain() {
-		if(this.valeur==1){
-			return "I";
-		}else if(this.valeur ==2)
-		{
-			return "II";
-		}else if(this.valeur == 3)
-		{
-			return "III";
-		}else
-		{
-			return "";
-		}
+		if(this.valeur==1){	return "I";}
+		else if(this.valeur ==2){ return "II";}
+		else if(this.valeur == 3){ return "III";}
+		else if(this.valeur == 4){ return "IV";}
+		else{ return "";}
     }
 }
--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -39,6 +39,7 @@ public class ApplicationTest {
 	assert_en_romain(1,"I");
 	assert_en_romain(2,"II");
 	assert_en_romain(3,"III");
+	assert_en_romain(4,"IV");
 
     }
 }
