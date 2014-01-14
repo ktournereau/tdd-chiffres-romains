8334a704948563e8905359fd75ed5a1b9fe2408f
LE JONCOUR Kévin <k.lejoncour@gmail.com>
2014-01-14 10:12:25 +0100

    Implémentation de la conversion de 4

--- app/models/NombreDecimal.java
+++ app/models/NombreDecimal.java
@@ -16,6 +16,8 @@ public class NombreDecimal {
     	if(this.valeur == 1) return "I";
     	if(this.valeur == 2) return "II";
     	if(this.valeur == 3) return "III";
+    	
+    	if(this.valeur == 4) return "IV";
 
     	return "";
     }
