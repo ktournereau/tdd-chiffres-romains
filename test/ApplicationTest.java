af8a283807be57d3d54835d96a8405a2db2024b0
LE JONCOUR Kévin <k.lejoncour@gmail.com>
2014-01-14 10:22:37 +0100

    Ajout du test assert_en_romain(5,'V');

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -40,7 +40,9 @@ public class ApplicationTest {
 	assert_en_romain(1,"I");
 	assert_en_romain(2,"II");
 	assert_en_romain(3,"III");
-	
+
 	assert_en_romain(4,"IV");
+
+	assert_en_romain(5,"V");
     }
 }
