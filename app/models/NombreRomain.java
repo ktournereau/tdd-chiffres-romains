4aef3d783aad475df8f69a0b916673596d44a2a6
LE JONCOUR Kévin <k.lejoncour@gmail.com>
2014-01-22 17:42:19 +0100

    Implémentation de la conversion des dizaines

--- app/models/NombreRomain.java
+++ app/models/NombreRomain.java
@@ -24,6 +24,16 @@ public class NombreRomain {
 		if(this.valeur == "VIII") return 8;	
 		if(this.valeur == "IX") return 9;
 
+		if(this.valeur == "X") return 10;
+		if(this.valeur == "XX") return 20;
+		if(this.valeur == "XXX") return 30;
+		if(this.valeur == "XL") return 40;
+		if(this.valeur == "L") return 50;
+		if(this.valeur == "LX") return 60;
+		if(this.valeur == "LXX") return 70;
+		if(this.valeur == "LXXX") return 80;	
+		if(this.valeur == "XC") return 90;
+
 		return 0;	
 	}
 
